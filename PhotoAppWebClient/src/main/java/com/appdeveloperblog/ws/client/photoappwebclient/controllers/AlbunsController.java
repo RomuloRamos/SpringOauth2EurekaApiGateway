01d7d2fdef8e2f0233eb4d47c20fe6d72be79da1
Romulo Ramos <romulo.ramos@gee.inatel.br>
2021-10-29 23:47:20 -0300

    Injecting WebClient on project to add access token to each request

--- PhotoAppWebClient/src/main/java/com/appdeveloperblog/ws/client/photoappwebclient/controllers/AlbunsController.java
+++ PhotoAppWebClient/src/main/java/com/appdeveloperblog/ws/client/photoappwebclient/controllers/AlbunsController.java
@@ -1,5 +1,6 @@
 package com.appdeveloperblog.ws.client.photoappwebclient.controllers;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,52 +25,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class AlbunsController {
     
     @Autowired
     OAuth2AuthorizedClientService oauth2ClientService;
+    // @Autowired
+    // RestTemplate restTemplate;
+
     @Autowired
-    RestTemplate restTemplate;
+    WebClient webClient;
 
     @GetMapping("/albuns")
     public String getAlbuns(Model model, @AuthenticationPrincipal OidcUser principal) {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
 
-        OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), 
-        oauthToken.getName());
+        // OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), 
+        // oauthToken.getName());
 
-        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
-        System.out.println("AccessToken: "+jwtAccessToken);
+        // String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+        // System.out.println("AccessToken: "+jwtAccessToken);
 
-        System.out.println("Principal: "+principal);
-        OidcIdToken idToken = principal.getIdToken();
-        String strIdToken = idToken.getTokenValue();
-        System.out.println("idTokenValue = "+strIdToken);
+        // System.out.println("Principal: "+principal);
+        // OidcIdToken idToken = principal.getIdToken();
+        // String strIdToken = idToken.getTokenValue();
+        // System.out.println("idTokenValue = "+strIdToken);
 
         String url = "http://localhost:8082/albuns"; //Route from Api Gateway
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer "+jwtAccessToken);
+        // HttpHeaders headers = new HttpHeaders();
+        // headers.add("Authorization", "Bearer "+jwtAccessToken);
 
-        HttpEntity<List<AlbunsRest>> entity = new HttpEntity<>(headers);
+        // HttpEntity<List<AlbunsRest>> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<List<AlbunsRest>> responseEntity =  restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<AlbunsRest>>() {});
-        List<AlbunsRest> albuns = responseEntity.getBody();
+        // ResponseEntity<List<AlbunsRest>> responseEntity =  restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<AlbunsRest>>() {});
+        // List<AlbunsRest> albuns = responseEntity.getBody();
+        List<AlbunsRest> albuns = webClient.get()
+            .uri(url)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<List<AlbunsRest>>(){})
+            .block();
         model.addAttribute("albuns", albuns);
-        // AlbunsRest album = new AlbunsRest();
-        // album.setAlbumId("albumOne");
-        // album.setAlbumTitle("Album One Title");
-        // album.setAlbumUrl("http://localhost:8082/albuns/1");
-
-        // AlbunsRest album2 = new AlbunsRest();
-        // album2.setAlbumId("albumTwo");
-        // album2.setAlbumTitle("Album Two Title");
-        // album2.setAlbumUrl("http://localhost:8082/albuns/2");
-
-        // model.addAttribute("albuns", Arrays.asList(album, album2));
 
         return "albuns";//Name of html file that should be called
     }
