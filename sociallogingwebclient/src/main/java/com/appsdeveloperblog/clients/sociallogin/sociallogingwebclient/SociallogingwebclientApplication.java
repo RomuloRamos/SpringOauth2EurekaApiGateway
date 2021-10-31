ddcf8031c8861f6791fcfae56db48f8cdae4ff4a
Romulo Ramos <romulo.ramos@gee.inatel.br>
2021-10-30 23:25:21 -0300

    Created an application that uses Social Login

---
+++ sociallogingwebclient/src/main/java/com/appsdeveloperblog/clients/sociallogin/sociallogingwebclient/SociallogingwebclientApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.sociallogin.sociallogingwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociallogingwebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociallogingwebclientApplication.class, args);
+	}
+
+}
---
+++ sociallogingwebclient/src/test/java/com/appsdeveloperblog/clients/sociallogin/sociallogingwebclient/SociallogingwebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.clients.sociallogin.sociallogingwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociallogingwebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
