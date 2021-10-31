6ab65742ba9974182eeb90241782031628a646ce
Romulo Ramos <romulo.ramos@gee.inatel.br>
2021-10-31 01:25:26 -0300

    Course finalized

--- resourceserver/src/main/java/com/appsdeveloperblog/resourceserver/security/WebSecurity.java
+++ resourceserver/src/main/java/com/appsdeveloperblog/resourceserver/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.appsdeveloperblog.resourceserver.security;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -20,7 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
         // logger.debug("Using default configure(HttpSecurity). If subclassed this will potentially override subclass conf");
-        http
+        http.cors().and()
             .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/users/status/check") //To any Get to /users
                 // .hasAuthority("SCOPE_profile") //check if have scope to profile
@@ -32,5 +38,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
             .oauth2ResourceServer()
             .jwt()
             .jwtAuthenticationConverter(jwtAuthenticationConverter);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
